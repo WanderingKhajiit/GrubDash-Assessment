6f3fdb0a00c6579a52543ac51ca56fc09d2473f2
Hunter Perkins <112034278+WanderingKhajiit@users.noreply.github.com>
2023-02-09 14:20:30 -0500

    Update dishes.router.js

--- src/dishes/dishes.router.js
+++ src/dishes/dishes.router.js
@@ -1,5 +1,18 @@
 const router = require("express").Router();
+const controller = require("./dishes.controller")
+const invalidMethod = require("../errors/methodNotAllowed")
 
+router.route("/:dishId")
+    .get(controller.read)
+    .post(controller.create)
+    .put(controller.update)
+    .all(invalidMethod)
+
+router.route("/")
+    .get(controller.list)
+    .post(controller.create)
+    .put(controller.update)
+    .all(invalidMethod)
 // TODO: Implement the /dishes routes needed to make the tests pass
 
 module.exports = router;
